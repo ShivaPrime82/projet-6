de63f30fb6c5ab98bdc982b4c6a7a5d36c9dbeeb
ShivaPrime82 <jeremy.clair.82@gmail.com>
2020-03-27 10:35:14 +0100

    add /api/comments and /api/articles

--- index.js
+++ index.js
@@ -13,4 +13,36 @@ app.listen(3000, () => {
 app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/api/articles", (req, res) => {
+    const sqlConnection = mysql.createConnection(sqlConfig);
 
+    sqlConnection.query(
+        "SELECT article_id, title, content, author, create_at FROM node_articles WHERE id = * LIMIT 5",
+        (error, result) => {
+            if (error) {
+                console.log("ERROR", error.code);
+            } else {
+                console.log("RESULT", result);
+                res.send(result[0]);
+            }
+            sqlConnection.end();
+        }
+    );
+});
+
+app.get("/api/comments", (req, res) => {
+    const sqlConnection = mysql.createConnection(sqlConfig);
+
+    sqlConnection.query(
+        "SELECT comments_id, article_id, content, author, create_at FROM node_comments WHERE id = * LIMIT 5",
+        (error, result) => {
+            if (error) {
+                console.log("ERROR", error.code);
+            } else {
+                console.log("RESULT", result);
+                res.send(result[0]);
+            }
+            sqlConnection.end();
+        }
+    );
+});
