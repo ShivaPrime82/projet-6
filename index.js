2c1dd2ca16447ff22ed3210b68794ad7d4ae8db1
ShivaPrime82 <jeremy.clair.82@gmail.com>
2020-03-27 10:45:50 +0100

    CD for articles

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+// Init import 
 const express = require("express");
 const mysql = require("mysql");
 const settings = require("./settings.json");
 
+// Settings for db
 const sqlConfig = settings.sqlConfig;
 
+// Init Server
 const app = express();
-
 app.listen(3000, () => {
     console.log("SERVER STARTED");
 });
 
+// Init static and urlencoded for later (post/get)
 app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: true }));
 
+// Init road for articles 
 app.get("/api/articles", (req, res) => {
     const sqlConnection = mysql.createConnection(sqlConfig);
 
@@ -30,6 +34,7 @@ app.get("/api/articles", (req, res) => {
     );
 });
 
+// Init road for comments 
 app.get("/api/comments", (req, res) => {
     const sqlConnection = mysql.createConnection(sqlConfig);
 
@@ -46,3 +51,58 @@ app.get("/api/comments", (req, res) => {
         }
     );
 });
+
+// INIT Create for articles
+app.route("/api/articles/create")
+    .get((req, res) => res.status(503).send({ status: "ERROR" }))
+    .post((req, res) => {
+
+        console.log(req.body);
+
+        const articles_title = req.body.articles_title;
+        const articles_content = req.body.articles_content;
+        const articles_author = req.body.articles_author;
+        const articles_create_at = req.body.articles_create_at;
+
+        const sqlConnection = mysql.createConnection(sqlConfig);
+
+        sqlConnection.query(
+            "INSERT INTO node_articles VALUES (NULL, ?, ?, ?, ?)",
+            [articles_title, articles_content, articles_author, articles_create_at],
+            (error, result) => {
+                if (error) {
+                    console.log("ERROR", error.code);
+                    res.status(503).send({ status: "ERROR" });
+                } else {
+                    res.send({ status: "OK" });
+                    console.log(result);
+                }
+                sqlConnection.end();
+            }
+        );
+    });
+
+// INIT Delete for articles
+app.route("/api/articles/delete")
+    .get((req, res) => res.status(503).send({ status: "ERROR" }))
+    .post((req, res) => {
+
+        const articles_id = req.body.articles_id;
+
+        const sqlConnection = mysql.createConnection(sqlConfig);
+
+        sqlConnection.query(
+            "DELETE FROM node_articles WHERE id = ?",
+            [articles_id],
+            (error, result) => {
+                if (error) {
+                    console.log("ERROR", error.code);
+                    res.status(503).send({ status: "ERROR" });
+                } else {
+                    res.send({ status: "OK" });
+                    console.log(result);
+                }
+                sqlConnection.end();
+            }
+        );
+    });
