d073f45c26e2db719f3cf4aa98419c5ea0cb4e0a
ShivaPrime82 <jeremy.clair.82@gmail.com>
2020-03-27 10:27:36 +0100

    index.js add express , mysql with setings and public folder static

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mysql = require("mysql");
+const settings = require("./settings.json");
+
+const sqlConfig = settings.sqlConfig;
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("SERVER STARTED");
+});
+
+app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: true }));
+
+
